674462dfb8308cbe72662f90faace79186094a44
leven <208cai5099@gmail.com>
2025-02-24 19:35:03 -0500

    Made Streamlit UI and updated Firestore

--- firebase_backend/firebase.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-# Use a service account.
-cred = credentials.Certificate('/Users/zhuobiaocai/Desktop/biology-review/firebase_backend/firebase_key.json')
-app = firebase_admin.initialize_app(cred)
-db = firestore.client()
